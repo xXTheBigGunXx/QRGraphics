282cc501d83107f84f6d9de22db85a2085fcd169
xXTheBigGunXx <144613981+xXTheBigGunXx@users.noreply.github.com>
2025-02-09 22:10:05 +0200

    New classes added to the project!

---
+++ GeneratorPoly.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GeneratorPoly
+{
+    private static Dictionary<int, int> map2;
+    private List<int> GeneratorPolynomial(int n)
+    {
+        map2 = GeneratorPoly.NumberWithN2();
+
+        List<Terms> terms = new List<Terms>();
+        Terms[] twoNumbersToMultiplie = new Terms[] { new Terms(0, 1), new Terms(0, 0) };
+        List<int> poly = new List<int>();
+
+        for(int i = 0; i < n; i++)
+        {
+            if (i == 0)
+            {
+                foreach (Terms item in twoNumbersToMultiplie)
+                {
+                    terms.Add(item);
+                }
+            }
+            else
+            {
+                terms = MultiplieForGalois(terms, twoNumbersToMultiplie);
+            }
+
+            twoNumbersToMultiplie[1] = new Terms(i+1, 0);
+            poly = CombineWithN(terms, i + 2);
+        }
+
+        /*Console.WriteLine(new string('!', 50));
+        poly.ForEach(x => Console.WriteLine(x));*/
+
+        return poly;
+    }
+    private static int Get(int index)
+    {
+        if(index == 255)
+        {
+            return 1;
+        }
+        index %= 255;
+        return map2[index];
+    }
+    private static List<int> CombineWithN(List<Terms> terms, int n)
+    {
+        List<int> powers = new List<int>(n);
+        
+        for(int i = 0; i < n; i++)
+        {
+            powers.Add(0);
+
+            for(int j = 0; j < terms.Count; j++)
+            {
+                if (n - i - 1 == terms[j].xPower)
+                {
+                    Terms b = terms[j];
+                    powers[i] ^= Get(b.aPower);
+                }
+            }
+        }
+        return powers;
+    }
+    private static List<Terms> MultiplieForGalois(List<Terms> terms, Terms[] twoNumbersToMultiplie)
+    {
+        List<Terms> tempList = new List<Terms>();
+
+        for(int i = 0; i < terms.Count; i++)
+        {
+            for(int j = 0; j < twoNumbersToMultiplie.Length; j++)
+            {
+                Terms temp = terms[i].Multiply(twoNumbersToMultiplie[j]);
+                tempList.Add(temp);    
+            }
+        }
+
+        return tempList;
+    }
+    private static Dictionary<int, int> NumberWithN2()
+    {
+        Dictionary<int, int> hashMap = new Dictionary<int, int>() { { 0, 1 } };
+        int curr = 1;
+
+        for (int i = 1; i < 255; i++)
+        {
+            curr *= 2;
+            if (curr > 255)
+            {
+                curr = curr ^ 285;
+            }
+            hashMap.Add(i, curr);
+        }
+        return hashMap;
+    }
+}
+
+
+
+
---
+++ Terms.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+public class Terms
+{
+     public int aPower {  get; set; }
+     public int xPower { get; set; }
+
+    public bool negativeA {  get; set; }
+
+     public Terms(int aPower, int xPower)
+     {
+        this.aPower = aPower;
+        this.xPower = xPower;
+     }
+
+    public override string ToString()
+    {
+        return String.Format($"a^{aPower} x^{xPower}");
+    }
+
+    public Terms Multiply(Terms a)
+    {
+        return new Terms(a.aPower + this.aPower, a.xPower + this.xPower);
+    }  
+    
+    public static Terms operator +(Terms a, Terms b)
+    {
+        return new Terms(a.aPower + b.aPower, a.xPower);
+    }
+
+    public static Terms operator -(Terms a, Terms b)
+    {
+        return new Terms(a.aPower - b.aPower, a.xPower);
+    }
+}
+
